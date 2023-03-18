e75a7ce3ff61b3c3d442aefe60d09982aa1a62b0
Julia <juliajun@uef.fi>
2023-03-18 13:32:06 +0200

    osa1 uusiksi

---
+++ Osa1uusi/part1/part1/src/App.js
@@ -0,0 +1,57 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  const Header = (props) => {
+    return (
+      <p>{props.course}</p>
+    )
+  }
+
+  /*const Content = (props) => {
+    return (
+      <div>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
+      </div>
+    )
+  }*/
+
+  const Total = (props) => {
+    return (
+      <p>Number of exercises {props.total}</p>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <p>{props.part} {props.exercises}</p>
+    )
+  }
+
+  return (
+    /*<div>
+      <Header course={course} />
+      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+    </div>*/
+    <div>
+      <Header course={course} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+    </div>
+  )
+}
+
+export default App
