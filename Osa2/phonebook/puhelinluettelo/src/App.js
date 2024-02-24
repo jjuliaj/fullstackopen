c1576a89d896becbfde5126b606ef099dd467714
Julia <juliajun@uef.fi>
2024-02-24 14:02:04 +0200

    2.9. toimii

--- Osa2/phonebook/puhelinluettelo/src/App.js
+++ Osa2/phonebook/puhelinluettelo/src/App.js
@@ -5,15 +5,21 @@ const App = (props) => {
   const [persons, setPersons] = useState(props.persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [showAll, setShowAll] = useState('')
+
+  const namesToShow = showAll
+    ? persons.filter(person => person.name.toLowerCase().includes(showAll.toLowerCase()))
+    : persons
+
 
   const addName = (event) => {
     event.preventDefault()
 
-    const isNameAlreadyAdded = persons.some(person => person.name === newName);
+    const isNameAlreadyAdded = persons.some(person => person.name === newName)
 
     if (isNameAlreadyAdded) {
-      alert(`"${newName}" on jo luettelossa!`);
-      return;
+      alert(`"${newName}" on jo luettelossa!`)
+      return
     }
 
     const nameNumberObject = {
@@ -27,7 +33,6 @@ const App = (props) => {
 
   }
 
-
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -36,9 +41,16 @@ const App = (props) => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setShowAll(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown names with: <input value={showAll} onChange={handleFilterChange} /></div>
+
+      <h2>Add new person</h2>
       <form onSubmit={addName}>
         <div>name: <input
           value={newName}
@@ -55,7 +67,7 @@ const App = (props) => {
       <h2>Numbers</h2>
       <div>
         <ul>
-          {persons.map(person =>
+          {namesToShow.map(person =>
             <Person key={person.id} person={person} />
           )}
         </ul>
