be5cac8f5589512be1811c074e375f8f6ff15de8
Julia <juliajun@uef.fi>
2024-02-24 13:20:14 +0200

    2.8. toimii

--- Osa2/phonebook/puhelinluettelo/src/App.js
+++ Osa2/phonebook/puhelinluettelo/src/App.js
@@ -4,6 +4,7 @@ import Person from './components/Person'
 const App = (props) => {
   const [persons, setPersons] = useState(props.persons)
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -15,28 +16,38 @@ const App = (props) => {
       return;
     }
 
-    const nameObject = {
+    const nameNumberObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1,
     }
-    setPersons([...persons, nameObject])
+
+    setPersons([...persons, nameNumberObject])
     setNewName('')
+
   }
+
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
-
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
-        <input
+        <div>name: <input
           value={newName}
           onChange={handleNameChange}
-        />
+        /></div>
+        <div>number: <input
+          value={newNumber}
+          onChange={handleNumberChange}
+        /></div>
         <div>
           <button type="submit">add</button>
         </div>
